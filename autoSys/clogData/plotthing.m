c816e961eb80ab17e554c44d7f657ab48fc98087
raaghavt <96094982+raaghav-t@users.noreply.github.com>
2024-10-01 18:25:23 -0700

    update

--- autoSys/clogData/plotthing.m
+++ autoSys/clogData/plotthing.m
@@ -1,19 +1,29 @@
-T = readmatrix('/Users/raaghavt/Desktop/UCSB/Summer/FiberClogging/fiber/autoSys/clogData/lightSignal_phi_30_ts500_20mlMin');
+close
+clear
+
+subplot(2,1,1)
+T = readmatrix('/Users/raaghavt/Desktop/UCSB/Summer/FiberClogging/fiber/autoSys/clogData/clogTimes_d_1o5mm(d)_phi_25_ts500_7o5mlMin_sept24');
 
 hold on
-i = (1:length(T))/4;
+i = (1:length(T));
 
 plot(i,T)
+plot(i, mean(T)*ones(length(T)), "LineStyle", '--', 'Color', [1 0 1])
+title('phi 25%')
+xlabel('nth point')
+ylabel('times')
+legend('data','avg')
 
-xlabel('time(s)')
-ylabel('average brightness')
-
-T = readmatrix('/Users/raaghavt/Desktop/UCSB/Summer/FiberClogging/fiber/autoSys/clogData/lightSignal_phi_30_ts500_2o5mlMin');
+subplot(2,1,2)
+T1 = readmatrix('/Users/raaghavt/Desktop/UCSB/Summer/FiberClogging/fiber/autoSys/clogData/clogTimes_d_1o5mm(d)_phi_35_ts500_7o5mlMin_sept24');
 
 hold on
-i = (1:length(T))/4;
+i = (1:length(T1));
 
-plot(i,T)
+plot(i,T1)
+plot(i, mean(T1)*ones(length(T1)), "LineStyle", '--', 'Color', [1 0 1])
+title('phi 35%')
+xlabel('nth point')
+ylabel('times')
 
-xlabel('time(s)')
-ylabel('average brightness')
+legend('data','avg')
